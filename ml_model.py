b479b2ad14b2e424c527f067d1bbe45bf8c134c5
jenifer2409 <jenifer2409@gmail.com>
2023-06-30 05:50:52 +0530

    adding 3 models

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("creating ml model file")
+
+def model1():
+    print("adding code for model1")
+
+def model2():
+    print("adding code for model2")
+
+def model3():
+    print("adding code for model3")
